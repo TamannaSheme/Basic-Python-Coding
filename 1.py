0f5a2dcdb8986b3667935f70bfdcc92b83912527
Syeda Tamanna Sheme <71142803+TamannaSheme@users.noreply.github.com>
2021-12-01 01:44:26 +0600

    Add files via upload
    Basic Python

---
+++ 1.py
@@ -0,0 +1,30 @@
+print("This is CSE366-ARTIFICIAL INTELLIGENCE.\
+       Fall 2021section 1.\
+       Total students is 36.")
+
+a = 10
+print(a)
+print(type(a))
+
+b = 10.10
+print(b)
+print(type(b))
+
+c = "Painkiller"
+print(c)
+print(type(c))
+
+f = 0B10
+print(f)
+
+
+g = 0X10
+print(g)
+
+h = 0B10
+print(f)
+
+i = 0O10
+print(i)
+
+
